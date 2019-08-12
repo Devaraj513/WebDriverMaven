eb18c5d7e069b3f7699345dc116aaec89200770c
devaraja <devarajsagar2015@gmail.com>
2019-08-12 16:36:55 +0530

    initial commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	
+	
+	@BeforeSuite
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "D://mysel//chromedriver.exe");
+		driver=new ChromeDriver();
+		
+	}
+	
+	@Test
+	public void doLogin() {
+		driver.get("http://www.gmail.com");
+		driver.findElement(By.cssSelector("#identifierId")).sendKeys("devraj@gmail.com");
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
